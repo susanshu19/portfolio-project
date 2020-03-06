e8b3cc5205ec14c91005f3c935ba58aa44345522
susanshu <dwivedisusanshu16@gmail.com>
2020-03-05 21:23:40 -0800

    blog functionalty

---
+++ blog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Blog(models.Model):
+    title =  models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to = 'images/',null = True)
+    def __str__(self):
+        return self.title
+
+    def summsry(self):
+        return self.body[:100] 
+
+    def pub_of_date(self):
+        return self.pub_date.strftime(' %b %e %Y ')    
---
+++ jobs/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Job(models.Model):
+    image = models.ImageField(upload_to = 'images/',null = True)
+    summary = models.CharField(max_length=20)
+   
+
+
+        
